42bff71c1a96e4fccfd8936205c062a00079bbe1
koouhz <kouhzj@gmail.com>
2025-08-17 19:08:38 -0400

    archivo de conexion

--- database/db.js
+++ database/db.js
@@ -1,1 +1,18 @@
+const mysql = require('mysql2');
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'tu_usuario',
+  password: 'tu_contraseña',
+  database: 'Proyecto'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err.message);
+    return;
+  }
+  console.log('Conectado a la base de datos MySQL!');
+});
+
+module.exports = connection;
