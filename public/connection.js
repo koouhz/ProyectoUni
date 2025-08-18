ab510ea205db8835757d384c8f8d47f0ac74e404
JhannelyCarvajal <jhanecarva@gmail.com>
2025-08-18 00:18:11 +0000

    Update connection.js

--- public/connection.js
+++ public/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'cc++4kglt',
+    password: 'Levitt20/05',
     database: 'Proyecto'
 });
 
